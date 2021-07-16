fd4b4bd3d6f64b6f2bb5466cbf6e1f832832192a
tabit <tabit@LAPTOP-6VNT2S1K.station>
2021-07-16 18:02:15 +0200

    Visita in profondita

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,11 +1,15 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -43,6 +47,50 @@ public class Model {
 		System.out.format("Grafo creato con %d vertici e %d archi\n",
 				this.grafo.vertexSet().size(), this.grafo.edgeSet().size()) ;
 //		System.out.println(this.grafo) ;
+		
+		//ora navighiamo il grafo 
+		
+		//per trovare i vertici adiacenti
+	//Fermata f;
+		
+		/*Set<DefaultEdge> archi = this.grafo.edgesOf(f);
+		
+		for (DefaultEdge e : archi) {
+			Graphs.getOppositeVertex(this.grafo, e, f);
+		}*/
+		
+		//alternativa 
+		
+	//List<Fermata> fermateAdiaceti = Graphs.successorListOf(this.grafo, f);
+	
+		
+		
+	}
+	
+	public List<Fermata> fermateRaggiungibili (Fermata partenza){
+		//BreadthFirstIterator<Fermata, DefaultEdge> bfv = new BreadthFirstIterator<>(this.grafo, partenza);
+		
+		DepthFirstIterator<Fermata, DefaultEdge> bfv = new DepthFirstIterator<>(this.grafo, partenza);
+		
+		List<Fermata>result = new ArrayList<>();
+		
+		while (bfv.hasNext()) {
+			Fermata f = bfv.next();
+			result.add(f);
+		}
+		
+		return result;
+		
+	}
+	
+	public Fermata trovaFermata (String nome) {
+		for (Fermata f : this.grafo.vertexSet()) {
+			if (f.getNome().equals(nome)) {
+				return f;
+			}
+		}
+		return null;
+		
 	}
 	
 }
--- src/main/java/it/polito/tdp/metroparis/model/TestModel.java
+++ src/main/java/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,11 +1,17 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 		Model m = new Model() ;
 		
 		m.creaGrafo(); 
+		Fermata p = m.trovaFermata("La Fourche");
+		List<Fermata> raggiungibili = m.fermateRaggiungibili(p);
+		
+		System.out.println(raggiungibili);
 
 	}
 
